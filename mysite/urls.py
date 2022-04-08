73d316761244d343cc8c4031b5bcd803749b8456
Le Viet Hoang <hoanglv@example.com>
2022-04-08 10:29:52 +0700

    update giao dien bai viet

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('home.urls'))
 ]
 
-handler404 = 'home.views.error'
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+handler404 = 'home.views.error'
